a6842b13e7d635ae5b02cc0163a701dc118b458f
bruno.sciolla <bruno.sciolla1@gmail.com>
2024-04-06 16:41:33 +0200

    Permettre un nombre défini de spawn

--- Assets/Scripts/Flocking/BoidSpawnerAuthoring.cs
+++ Assets/Scripts/Flocking/BoidSpawnerAuthoring.cs
@@ -10,6 +10,8 @@ public class BoidSpawnerAuthoring : MonoBehaviour
 {
     public float interval = 0.3f;
     public float angle_range = 30;
+    public int maxSpawn = 10;
+    public int countSpawn = 0;
     public BoidAuthoring prefab;
 
     public class Baker: Baker<BoidSpawnerAuthoring>
@@ -24,6 +26,8 @@ public class BoidSpawnerAuthoring : MonoBehaviour
                 prefab = GetEntity(authoring.prefab, TransformUsageFlags.Dynamic),
                 time = 0,
                 random = new Unity.Mathematics.Random(10),
+                maxSpawn = authoring.maxSpawn,
+                countSpawn = authoring.countSpawn,
             });
         }
     }
@@ -36,6 +40,8 @@ public struct BoidSpawner: IComponentData
     public Entity prefab;
     public float time;
     public Unity.Mathematics.Random random;
+    public int maxSpawn;
+    public int countSpawn;
 }
 
 public partial class BoidSpawnerUpdateSystem: SystemBase 
@@ -47,8 +53,9 @@ public partial class BoidSpawnerUpdateSystem: SystemBase
             .ForEach((EntityCommandBuffer command_buffer, ref BoidSpawner spawner, in LocalTransform transform) =>
         {
             spawner.time += dt;
-            if(spawner.time > spawner.interval)
+            if(spawner.time > spawner.interval && spawner.countSpawn <= spawner.maxSpawn)
             {
+                spawner.countSpawn++;
                 spawner.time -= spawner.interval;
                 Entity boid = command_buffer.Instantiate(spawner.prefab);
                 command_buffer.SetComponent<LocalTransform>(boid, new LocalTransform
