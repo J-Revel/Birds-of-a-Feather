70dbb27f8e7013cd7e44de529cf80883d8a7f565
bruno.sciolla <bruno.sciolla1@gmail.com>
2024-04-06 11:43:48 +0200

    Utilise l'accélération pour les boids

--- Assets/Scripts/Flocking/BoidAuthoring.cs
+++ Assets/Scripts/Flocking/BoidAuthoring.cs
@@ -13,16 +13,16 @@ public class BoidAuthoring : MonoBehaviour
     public BoidBehaviourConfig config;
     public float start_direction_angle;
 
-    public class Baker: Baker<BoidAuthoring>
+    public class Baker : Baker<BoidAuthoring>
     {
         public override void Bake(BoidAuthoring authoring)
         {
             Entity entity = GetEntity(TransformUsageFlags.Dynamic);
             DependsOn(authoring.config);
             AddComponent<BoidState>(entity,
-                new BoidState 
+                new BoidState
                 {
-                    velocity = new float2(math.cos(authoring.start_direction_angle), math.sin(authoring.start_direction_angle)) 
+                    velocity = new float2(math.cos(authoring.start_direction_angle), math.sin(authoring.start_direction_angle))
                 });
             AddComponent<BoidConfig>(entity, new BoidConfig
             {
@@ -39,7 +39,7 @@ public class BoidAuthoring : MonoBehaviour
     }
 }
 
-public struct BoidConfig: IComponentData
+public struct BoidConfig : IComponentData
 {
     public float speed;
     public float attraction_force;
@@ -51,24 +51,25 @@ public struct BoidConfig: IComponentData
     public float align_force;
 }
 
-public struct BoidState: IComponentData
+public struct BoidState : IComponentData
 {
     public float2 velocity;
+    public float2 acceleration;
 }
 
-public struct BoidPartitionCell: IComponentData
+public struct BoidPartitionCell : IComponentData
 {
     public int2 partition;
 }
 
-public struct BoidNeighbourData: IComponentData
+public struct BoidNeighbourData : IComponentData
 {
     public float2 average_velocity;
     public int neighbour_count;
 }
 
 [UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
-public partial class BoidMovementSystem: SystemBase
+public partial class BoidMovementSystem : SystemBase
 {
     public NativeParallelMultiHashMap<int2, Entity> partition_grid;
     public float partition_size = 5;
@@ -90,23 +91,23 @@ public partial class BoidMovementSystem: SystemBase
         Entities.WithDeferredPlaybackSystem<EndFixedStepSimulationEntityCommandBufferSystem>()
             .WithNone<BoidPartitionCell>()
             .ForEach((EntityCommandBuffer command_buffer, Entity entity, in LocalTransform transform, in BoidState state, in BoidConfig config) =>
-        {
-            int2 cell = (int2)(transform.Position.xz / partition_size);
-            command_buffer.AddComponent<BoidPartitionCell>(entity, new BoidPartitionCell { partition = cell });
-            partition.Add(cell, entity);
-        }).Schedule();
+            {
+                int2 cell = (int2)(transform.Position.xz / partition_size);
+                command_buffer.AddComponent<BoidPartitionCell>(entity, new BoidPartitionCell { partition = cell });
+                partition.Add(cell, entity);
+            }).Schedule();
         Entities
             .WithName("Position_Update")
             .ForEach((Entity entity, ref LocalTransform transform, ref BoidState state, ref BoidPartitionCell partition_cell, in BoidConfig config) =>
-        {
-            transform.Position += new float3(state.velocity.x, 0, state.velocity.y) * dt;
-            int2 new_partition_cell = (int2)(transform.Position.xz / partition_size);
-            if(math.lengthsq(partition_cell.partition - new_partition_cell) > 0)
             {
-                partition.Remove(partition_cell.partition, entity);
-                partition.Add(new_partition_cell, entity);
-            }
-        }).Schedule();
+                transform.Position += new float3(state.velocity.x, 0, state.velocity.y) * dt;
+                int2 new_partition_cell = (int2)(transform.Position.xz / partition_size);
+                if (math.lengthsq(partition_cell.partition - new_partition_cell) > 0)
+                {
+                    partition.Remove(partition_cell.partition, entity);
+                    partition.Add(new_partition_cell, entity);
+                }
+            }).Schedule();
         var writer = partition.AsParallelWriter();
         Entities.WithName("Partition_Update").ForEach((Entity entity, in LocalTransform transform, in BoidState state) =>
         {
@@ -117,70 +118,72 @@ public partial class BoidMovementSystem: SystemBase
             .WithName("Neighbour_Data_Compute")
             .WithReadOnly(partition)
             .ForEach((Entity entity, ref BoidNeighbourData neighbour_data, in BoidState state, in LocalTransform transform, in BoidPartitionCell partition_cell, in BoidConfig config) =>
-        {
-            float2 position = transform.Position.xz;
-
-            float max_range = config.neighbour_detection_range;
-            int2 min_partition = (int2)((position - max_range) / partition_size);
-            int2 max_partition = (int2)((position + max_range) / partition_size);
-
-            float2 neighbour_velocity_sum = float2.zero;
-            int neighbour_count = 0;
-            for(int i=min_partition.x; i <= max_partition.x; i++)
             {
-                for(int j=min_partition.y; j<=max_partition.y; j++)
+                float2 position = transform.Position.xz;
+
+                float max_range = config.neighbour_detection_range;
+                int2 min_partition = (int2)((position - max_range) / partition_size);
+                int2 max_partition = (int2)((position + max_range) / partition_size);
+
+                float2 neighbour_velocity_sum = float2.zero;
+                int neighbour_count = 0;
+                for (int i = min_partition.x; i <= max_partition.x; i++)
                 {
-                    foreach(Entity neighbour_entity in partition.GetValuesForKey(new int2(i, j)))
+                    for (int j = min_partition.y; j <= max_partition.y; j++)
                     {
-                        if (neighbour_entity == entity)
-                            continue;
-                        neighbour_velocity_sum += SystemAPI.GetComponent<BoidState>(neighbour_entity).velocity;
-                        neighbour_count++;
+                        foreach (Entity neighbour_entity in partition.GetValuesForKey(new int2(i, j)))
+                        {
+                            if (neighbour_entity == entity)
+                                continue;
+                            neighbour_velocity_sum += SystemAPI.GetComponent<BoidState>(neighbour_entity).velocity;
+                            neighbour_count++;
+                        }
                     }
                 }
-            }
-            if(neighbour_count > 0)
-            {
-                neighbour_data.average_velocity = neighbour_velocity_sum / neighbour_count;
-                neighbour_data.neighbour_count = neighbour_count;
+                if (neighbour_count > 0)
+                {
+                    neighbour_data.average_velocity = neighbour_velocity_sum / neighbour_count;
+                    neighbour_data.neighbour_count = neighbour_count;
 
-            }
-        }).ScheduleParallel();
+                }
+            }).ScheduleParallel();
         Entities
             .WithName("Attraction_Repulsion")
             .WithReadOnly(partition)
             .ForEach((Entity entity, ref BoidState state, in LocalTransform transform, in BoidPartitionCell partition_cell, in BoidConfig config, in BoidNeighbourData neighbour_data) =>
-        {
-            float2 position = transform.Position.xz;
-
-            float max_range = math.max(config.attraction_range, config.repulsion_range);
-            int2 min_partition = (int2)((position - max_range) / partition_size);
-            int2 max_partition = (int2)((position + max_range) / partition_size);
-            for(int i=min_partition.x; i <= max_partition.x; i++)
             {
-                for(int j=min_partition.y; j<=max_partition.y; j++)
+                state.acceleration = new float2();
+                float2 position = transform.Position.xz;
+
+                float max_range = math.max(config.attraction_range, config.repulsion_range);
+                int2 min_partition = (int2)((position - max_range) / partition_size);
+                int2 max_partition = (int2)((position + max_range) / partition_size);
+                for (int i = min_partition.x; i <= max_partition.x; i++)
                 {
-                    foreach(Entity neighbour_entity in partition.GetValuesForKey(new int2(i, j)))
+                    for (int j = min_partition.y; j <= max_partition.y; j++)
                     {
-                        if (neighbour_entity == entity)
-                            continue;
-                        float2 neighbour_position = SystemAPI.GetComponent<LocalTransform>(neighbour_entity).Position.xz;
-                        float2 direction = math.all(neighbour_position == position) ? float2.zero : math.normalize(neighbour_position - position);
-                        float distancesq = math.distancesq(position, neighbour_position);
-                        if(distancesq < config.attraction_range)
+                        foreach (Entity neighbour_entity in partition.GetValuesForKey(new int2(i, j)))
                         {
-                            state.velocity += direction * dt * config.attraction_force;
-                        }
-                        if(distancesq < config.repulsion_range)
-                        {
-                            state.velocity -= direction * dt * config.repulsion_force;
+                            if (neighbour_entity == entity)
+                                continue;
+                            float2 neighbour_position = SystemAPI.GetComponent<LocalTransform>(neighbour_entity).Position.xz;
+                            float2 direction = math.all(neighbour_position == position) ? float2.zero : math.normalize(neighbour_position - position);
+                            float distancesq = math.distancesq(position, neighbour_position);
+                            if (distancesq < config.attraction_range)
+                            {
+                                state.acceleration += direction * config.attraction_force;
+                            }
+                            if (distancesq < config.repulsion_range)
+                            {
+                                state.acceleration += -direction * config.repulsion_force;
+                            }
                         }
                     }
                 }
-            }
-            if(math.all(neighbour_data.average_velocity != float2.zero))
-                state.velocity += math.normalize(neighbour_data.average_velocity) * config.align_force * dt;
-            state.velocity = math.normalize(state.velocity) * config.speed;
-        }).ScheduleParallel();
+                if (math.all(neighbour_data.average_velocity != float2.zero))
+                    state.acceleration += math.normalize(neighbour_data.average_velocity) * config.align_force;
+                state.velocity = state.velocity + state.acceleration * dt;
+                state.velocity = math.normalize(state.velocity) * config.speed;
+            }).ScheduleParallel();
     }
 }
